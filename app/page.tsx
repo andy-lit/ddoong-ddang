08954584d6e8ab0661c8c1b6349dd6027aae9962
naamoonoo <naamoonoo.y@gmail.com>
2024-11-27 13:43:51 +0900

    feat: 지도 정보 분기

--- app/page.tsx
+++ app/page.tsx
@@ -27,7 +27,7 @@ export default function Home() {
             //   pauseOnMouseEnter: true,
             //   stopOnLastSlide: true,
             // }}
-            className="w-[100vw] h-[60vh] sm:w-[390px] sm:h-[60vh] z-2"
+            className="w-[100vw] h-[65vh] sm:w-[390px] sm:h-[60vh] z-2"
           >
             {[1, 2, 3, 4, 5, 6, 7, 1].map((item, idx) => (
               <SwiperSlide key={item + idx}>
@@ -46,28 +46,53 @@ export default function Home() {
           <div className="text-sm mt-2 text-center">
             2024년 12월 14일 오후 7시 30분
           </div>
-          <a
-            href="nmap://search?query=홍대 우주정거장&appname=뚱땅뚱땅밴드"
-            onClick={(e) => {
-              e.preventDefault();
-              // 모바일에서는 네이버 지도 앱으로, PC에서는 웹으로 연결
-              const naverMapUrl = /Android|iPhone|iPad|iPod/i.test(
-                navigator.userAgent
-              )
-                ? "nmap://search?query=홍대 우주정거장&appname=뚱땅뚱땅밴드"
-                : "https://map.naver.com/p/search/홍대%20우주정거장";
-              window.location.href = naverMapUrl;
-            }}
-            className="text-sm mt-1 text-center block hover:text-blue-500 cursor-pointer"
-          >
-            @홍대 우주정거장
-          </a>
+          <div className="flex items-center justify-center gap-2">
+            <a
+              href="nmap://search?query=홍대 우주정거장&appname=뚱땅뚱땅밴드"
+              onClick={(e) => {
+                e.preventDefault();
+                const naverMapUrl = /Android|iPhone|iPad|iPod/i.test(
+                  navigator.userAgent
+                )
+                  ? "nmap://search?query=홍대 우주정거장&appname=뚱땅뚱땅밴드"
+                  : "https://map.naver.com/p/search/홍대%20우주정거장";
+                window.location.href = naverMapUrl;
+              }}
+              className="flex items-center gap-1 text-sm mt-1 hover:text-blue-500 cursor-pointer"
+            >
+              <span>@홍대 우주정거장</span>
+              <img
+                src="https://i.namu.wiki/i/g1ObOjgHeGx6qsTX-DgwrwyHL8uHBhXxiPQTCu9w5M32o0po4v1ugu_ikoEIncrVO-kq3Q73lCs8MzRgH55G2A.webp"
+                alt="네이버 지도"
+                className="w-5 h-5"
+              />
+            </a>
+            <a
+              href="kakaomap://search?q=홍대 우주정거장"
+              onClick={(e) => {
+                e.preventDefault();
+                const kakaoMapUrl = /Android|iPhone|iPad|iPod/i.test(
+                  navigator.userAgent
+                )
+                  ? "kakaomap://search?q=홍대 우주정거장"
+                  : "https://map.kakao.com/?q=홍대 우주정거장";
+                window.location.href = kakaoMapUrl;
+              }}
+              className="flex items-center gap-1 text-sm mt-1 hover:text-yellow-500 cursor-pointer"
+            >
+              <img
+                src="https://play-lh.googleusercontent.com/pPTTNz433EYFurg2j__bFU5ONdMoU_bs_-yS2JLZriua3iHrksGP6XBPF5VtDPlpGcW4"
+                alt="카카오 지도"
+                className="w-5 h-5"
+              />
+            </a>
+          </div>
 
           {/* Dynamic Island */}
-          <div className="absolute top-3 z-[5] left-1/2 transform -translate-x-1/2 w-[130px] h-[30px] bg-black rounded-[30px] border border-gray-900 opacity-50" />
+          <div className="hidden sm:block absolute top-3 z-[5] left-1/2 transform -translate-x-1/2 w-[130px] h-[30px] bg-black rounded-[30px] border border-gray-900 opacity-50" />
 
           {/* Gesture Bar */}
-          <div className="absolute bottom-3  z-[5] left-1/2 transform -translate-x-1/2 w-[140px] h-[5px] bg-black rounded-full" />
+          <div className="hidden sm:block absolute bottom-3  z-[5] left-1/2 transform -translate-x-1/2 w-[140px] h-[5px] bg-black rounded-full" />
         </div>
       </div>
       {/* 
