4b7998565bb83e50507e160a8a683800ab253a25
naamoonoo <naamoonoo.y@gmail.com>
2024-12-02 15:49:52 +0900

    wip

--- app/userInfo.tsx
+++ app/userInfo.tsx
@@ -3,43 +3,36 @@ export const userInfo = [
   {
     name: "관중",
     session: "Drum",
-    imageUrl: "/andy.png",
-    instagram: "froglimber",
+    instagram: "kakarou_",
   },
   {
     name: "승은",
     session: "Vocal",
-    imageUrl: "/andy.png",
     instagram: "froglimber",
   },
   {
     name: "유경",
     session: "Guitar",
-    imageUrl: "/andy.png",
-    instagram: "froglimber",
+    instagram: "k_kyung_g",
   },
   {
     name: "현우",
     session: "Vocal",
-    imageUrl: "/andy.png",
     instagram: "froglimber",
   },
   {
     name: "수진",
     session: "Bass",
-    imageUrl: "/andy.png",
     instagram: "froglimber",
   },
   {
     name: "진우",
     session: "Keyboard",
-    imageUrl: "/andy.png",
     instagram: "froglimber",
   },
   {
     name: "바름",
     session: "Guitar",
-    imageUrl: "/andy.png",
-    instagram: "froglimber",
+    instagram: "ba_rreum",
   },
 ];
